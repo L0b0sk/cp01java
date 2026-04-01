591a11c4a1a1ef336faa9f35a56ece30ed9bcc3f
L0b0sk <brulobosque@gmail.com>
2026-04-01 12:16:44 -0300

    fazendo todos os imports para funcionamento do codigo

--- ATMfiap.java
+++ ATMfiap.java
@@ -1,2 +1,4 @@
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.Scanner;       // Importa a classe Scanner para ler dados do teclado
+import java.util.ArrayList; // Importa a classe ArrayList para armazenar o histórico de transações
+import java.time.LocalDateTime; // Importa as classes para trabalhar com data e hora
+import java.time.format.DateTimeFormatter;
